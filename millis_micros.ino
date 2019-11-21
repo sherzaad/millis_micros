5526b1d1712e99351ca9b0b415e53cff538ef908
sherzaad <57634578+sherzaad@users.noreply.github.com>
2019-11-21 11:54:17 +0000

    Add files via upload

---
+++ millis_micros.ino
@@ -0,0 +1,34 @@
+#include "ms_us_Timer.h"
+
+mstimer ms_t1(500); //create a 500ms timer
+mstimer ms_t2(1000); //create a 1000ms timer
+uint8_t i = 0;
+
+void setup() {
+
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+
+}
+
+void loop() {
+  
+  if (ms_t1.HasElapsed()) {
+    if (i == 50) {
+      Serial.println("t1 disabled");
+      digitalWrite(13, LOW);
+      ms_t1.ResetTime(0); //disable t1. could have also used this function to change the timer period
+    }
+    else {
+      digitalWrite(13, !digitalRead(13));
+      Serial.println(digitalRead(13));
+    }
+    ++i;
+  }
+  else if (ms_t2.HasElapsed()) {
+    Serial.println("t2 triggered!");
+  }
+
+}
---
+++ ms_us_Timer.h
@@ -0,0 +1,88 @@
+#ifndef	MS_US_TIMER_H
+#define	MS_US_TIMER_H
+
+/*ms timer using macro with example
+
+  #define msTimer(x)  for(static unsigned long pm = millis(); millis() - pm >= x; pm = millis())
+
+  msTimer(500) {
+    digitalWrite(13, !digitalRead(13));
+    Serial.print("Blink @ ");
+    Serial.println(millis());
+  }
+
+*/
+
+//millisecond timer class using millis()
+class mstimer {
+  private:
+    unsigned long _oldtime;
+    unsigned long _period;
+
+  public:
+    //returns if period has elapsed or not
+    inline uint8_t HasElapsed()
+    {
+      if (_period > 0 && millis() - _oldtime >= _period) {
+        _oldtime = millis();
+        return 1;
+      }
+      
+	  return 0;
+    }
+
+    //update period
+    inline void ResetTime(unsigned long period) {
+      //using setTime to set period=0 essentially stops the instated timer
+      _period = period;
+      _oldtime = millis();
+    }
+
+    //constructor
+    mstimer(unsigned long period = 0)
+    {
+      _oldtime = millis();
+      _period = period;
+    }
+
+    //deconstructor
+    ~mstimer() {}
+};
+
+//microsecond timer class using micros()
+class ustimer {
+  private:
+    unsigned long _oldtime;
+    unsigned long _period;
+
+  public:
+    //returns if time has elapsed or not
+    inline uint8_t HasElapsed()
+    {
+      if (_period > 0 && micros() - _oldtime >= _period) {
+        _oldtime = micros();
+        return 1;
+      }
+      
+	  return 0;
+    }
+
+    //update period
+    inline void ResetTime(unsigned long period) {
+      //using setTime to set period=0 essentially stops the instated timer
+      _period = period;
+      _oldtime = micros();
+    }
+
+    //constructor
+    ustimer(unsigned long period = 0)
+    {
+      _oldtime = micros();
+      _period = period;
+    }
+
+    //deconstructor
+    ~ustimer() {}
+};
+
+#endif
